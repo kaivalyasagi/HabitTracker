b7d5eb12ed7dcb6b8f858b43f953205ead51012d
Kaivalya Sagi <kaivalyasagi@Kaivalyas-MacBook-Air.local>
2026-03-22 12:33:09 +0530

    fix cors origin

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ app = FastAPI(title="Bloom API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://habit-tracker-eta-rose.vercel.app" ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
